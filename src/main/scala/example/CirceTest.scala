0e176d5aebc19fe08052af8a26b955b288c0e9c3
yaxin.wang <yaxin.wang@rea-group.com>
2020-03-21 21:14:14 +0800

    [Circe]Add Parsing JSON.

--- src/main/scala/example/CirceTest.scala
+++ src/main/scala/example/CirceTest.scala
@@ -3,9 +3,17 @@ package example
 import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
+import io.circe.{Json, ParsingFailure}
 
 object CirceTest {
+
   def main(args: Array[String]): Unit = {
+//    testBasicCirceInfo()
+
+    testParseJson()
+  }
+
+  private def testBasicCirceInfo(): Unit = {
     val foo: Foo = Qux(13, Some(14.0))
 
     val json = foo.asJson.noSpaces
@@ -13,6 +21,22 @@ object CirceTest {
 
     val decodedFoo = decode[Foo](json)
     println(decodedFoo)
+  }
+
+  def testParseJson(): Unit = {
+    val rawJson: String = """
+        {
+          "foo": "bar",
+          "baz": 123,
+          "list of stuff": [ 4, 5, 6 ]
+        }
+        """
+    val parseResult: Either[ParsingFailure, Json] = parse(rawJson)
+    val jsonResult: Json = parseResult.getOrElse(Json.Null)
+    println(jsonResult)
+
+    val badJson: String = "hahahah"
+    println(parse(badJson).getOrElse(Json.Null))
   }
 }
 
