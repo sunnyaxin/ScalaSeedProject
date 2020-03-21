cf1ce671c527ee538e16cc64ddf2d871b14cc5cc
sunnyaxin <137126846@qq.com>
2020-03-21 21:33:55 +0800

    [Circe]Add test traverse and modify json.

--- src/main/scala/example/CirceTest.scala
+++ src/main/scala/example/CirceTest.scala
@@ -1,16 +1,18 @@
 package example
 
+import io.circe._
 import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
-import io.circe.{Json, ParsingFailure}
 
 object CirceTest {
 
   def main(args: Array[String]): Unit = {
 //    testBasicCirceInfo()
 
-    testParseJson()
+//    testParseJson()
+
+    testTraverseAndModifyJson()
   }
 
   private def testBasicCirceInfo(): Unit = {
@@ -37,6 +39,41 @@ object CirceTest {
 
     val badJson: String = "hahahah"
     println(parse(badJson).getOrElse(Json.Null))
+  }
+
+  def testTraverseAndModifyJson(): Unit = {
+    val json: String = """
+      {
+        "id": "c730433b-082c-4984-9d66-855c243266f0",
+        "name": "Foo",
+        "counts": [1, 2, 3],
+        "values": {
+          "bar": true,
+          "baz": 100.001,
+          "qux": ["a", "b"]
+        }
+      }
+    """
+
+    val doc: Json = parse(json).getOrElse(Json.Null)
+
+    // Extracting data - HCursor
+    val cursor: HCursor = doc.hcursor
+    val baz: Decoder.Result[Double] =
+      cursor.downField("values").downField("baz").as[Double]
+    val baz2 = cursor.downField("values").get[Double]("baz")
+    val qux = cursor.downField("values").downField("qux").downArray.as[String]
+    println(baz + " " + baz2 + " " + qux)
+
+    //Transforming data - ACursor
+    val reversedNameCursor: ACursor =
+      cursor.downField("name").withFocus(_.mapString(_.reverse))
+    val reversedName: Option[Json] = reversedNameCursor.top
+    println(reversedName)
+
+//    Cursor provides functionality for moving around a tree and making modifications
+//    HCursor tracks the history of operations performed. This can be used to provide useful error messages when something goes wrong.
+//    ACursor also tracks history, but represents the possibility of failure (e.g. calling downField on a field that doesn’t exist)
   }
 }
 
