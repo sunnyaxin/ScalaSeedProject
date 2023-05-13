d2093d38b736f1147bb1d0ed515339eb6e60c7d5
Yaxin <137126846@qq.com>
2023-05-13 20:46:13 +0800

    [problems] Add merge two sorted lists.

---
+++ src/main/scala/problems/MergeTwoSortedLists.scala
@@ -0,0 +1,51 @@
+package problems
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+class ListNode(_x: Int = 0, _next: ListNode = null) {
+  var next: ListNode = _next
+  var x: Int = _x
+}
+
+object MergeTwoSortedLists extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- IO(
+        println(
+          Solution.mergeTwoLists(
+            new ListNode(1, new ListNode(2, new ListNode(4))),
+            new ListNode(1, new ListNode(3, new ListNode(4)))
+          )
+        )
+      )
+      _ <- IO(
+        println(
+          Solution.mergeTwoLists(null, null)
+        )
+      )
+      _ <- IO(
+        println(
+          Solution.mergeTwoLists(new ListNode(0), null)
+        )
+      )
+    } yield ExitCode.Success
+
+  object Solution {
+
+//    @tailrec
+    def merge(list1: ListNode, list2: ListNode, temp: ListNode): ListNode = {
+      if (list1 == null) { temp.next = list2; return temp }
+      if (list2 == null) { temp.next = list1; return temp }
+      if (list1.x < list2.x) {
+        temp.next = list1; return merge(list1.next, list2, list1)
+      } else temp.next = list2; merge(list1, list2.next, list2)
+    }
+
+    def mergeTwoLists(list1: ListNode, list2: ListNode): ListNode = {
+      val init = new ListNode()
+      merge(list1, list2, init)
+      init.next
+    }
+  }
+}
