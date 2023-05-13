b7c6de785a7159fdf1c87f0ba402c632dff011f8
Yaxin <137126846@qq.com>
2023-05-13 22:04:07 +0800

    [problems] Add remove duplicates.

---
+++ src/main/scala/problems/RemoveDuplicates.scala
@@ -0,0 +1,37 @@
+package problems
+
+import scala.collection.mutable.ArrayBuffer
+
+object RemoveDuplicates {
+  def main(args: Array[String]): Unit = {
+    println(Solution.removeDuplicates(Array(1, 1, 2)))
+    println(Solution.removeDuplicates(Array(0, 0, 1, 1, 1, 2, 2, 3, 3, 4)))
+  }
+
+  object Solution {
+    def removeDuplicates1(nums: Array[Int]): Int = {
+      val result = new ArrayBuffer[Int]
+      for (num <- nums if !result.contains(num)) {
+        result += num
+      }
+      for (i <- result.indices) {
+        nums(i) = result(i)
+      }
+      result.length
+    }
+
+    def removeDuplicates2(nums: Array[Int]): Int = {
+      val newNums = nums.distinct
+      for (i <- newNums.indices) {
+        nums(i) = newNums(i)
+      }
+      newNums.length
+    }
+
+    def removeDuplicates(nums: Array[Int]): Int = {
+      val newNums = nums.distinct
+      newNums.zipWithIndex.foreach { case (item, index) => nums(index) = item }
+      newNums.length
+    }
+  }
+}
