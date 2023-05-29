f00668de76574e50cfbc7e15c42be4f87f268529
Yaxin <137126846@qq.com>
2023-05-29 21:40:22 +0800

    [problems] Add pascal triangle get row.

--- src/main/scala/problems/PascalTriangle.scala
+++ src/main/scala/problems/PascalTriangle.scala
@@ -2,8 +2,11 @@ package problems
 
 object PascalTriangle {
   def main(args: Array[String]): Unit = {
-    println(Solution.generate(5)) // [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
-    println(Solution.generate(1)) //[[1]]
+//    println(Solution.generate(5)) // [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+//    println(Solution.generate(1)) //[[1]]
+    println(Solution.getRow(3)) //[1,3,3,1]
+    println(Solution.getRow(0)) //[1]
+    println(Solution.getRow(1)) //[1,1]
   }
 
   object Solution {
@@ -19,6 +22,21 @@ object PascalTriangle {
         }
       }
       triangle.map(_.toList).toList
+    }
+
+    private def row(prev: List[Int]): List[Int] = {
+      1 :: prev.sliding(2).map(_.sum).toList ::: List(1)
+    }
+
+    def getRow(rowIndex: Int): List[Int] = {
+      if (rowIndex == 0) return List(1)
+      var id = 1
+      var prev = List(1, 1)
+      while (id < rowIndex) {
+        prev = row(prev)
+        id += 1
+      }
+      prev
     }
   }
 }
