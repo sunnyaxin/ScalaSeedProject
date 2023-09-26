f4cedadc43e15e54730782be1b6fb99eab7a6137
Yaxin <137126846@qq.com>
2023-09-26 19:34:36 +0800

    [Practice] Add for synax test.

--- src/main/scala/example/RaiseerrorTest.scala
+++ src/main/scala/example/RaiseerrorTest.scala
@@ -2,11 +2,21 @@ package example
 
 import cats.effect.{ExitCode, IO, IOApp}
 
-object RaiseerrorTest extends IOApp{
-  override def run(args: List[String]): IO[ExitCode] = for {
-    _ <- IO.println("1")
-    _ <- IO.println("2")
-    _ <- IO.raiseError(new Error("error")).recover { case _: Throwable => ()}
-    _ <- IO.println("4")
-  } yield ExitCode.Success
+object RaiseerrorTest extends IOApp {
+//  override def run(args: List[String]): IO[ExitCode] = for {
+//    _ <- IO.println("1")
+//    _ <- IO.println("2")
+//    _ <- IO.raiseError(new Error("error")).recover { case _: Throwable => ()}
+//    _ <- IO.println("4")
+//  } yield ExitCode.Success
+
+  override def run(args: List[String]): IO[ExitCode] =
+    IO.println("1")
+      .flatMap(_ =>
+        IO.println("2")
+          .flatMap(_ =>
+            IO.println("4")
+              .map(_ => ExitCode.Success)
+          )
+      )
 }
